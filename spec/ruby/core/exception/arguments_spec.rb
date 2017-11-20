93ce4edafe288ada705c7ae682da64a38f489a95
esjee <ewfawefw3@gmail.com>
2017-11-20 22:33:40 +0100

    add spec

--- spec/ruby/core/exception/arguments_spec.rb
+++ spec/ruby/core/exception/arguments_spec.rb
@@ -1,11 +1,28 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe "ArgumentError" do
+  class ArgumentErrorDummyClass
+    def foo(a,b,c:)
+    end
+  end
+
   it "is a subclass of StandardError" do
     StandardError.should be_ancestor_of(ArgumentError)
   end
 
   it "gives its own class name as message if it has no message" do
     ArgumentError.new.message.should == "ArgumentError"
+  end
+
+  it "includes receiver and method name when raised by application code" do
+    exc = begin
+      ArgumentErrorDummyClass.new.foo(3)
+    rescue => exc
+      exc
+    end
+
+    exc.instance_variable_get('@receiver').should_not be_nil
+    exc.instance_variable_get('@receiver').should.class === ArgumentErrorDummyClass
+    exc.instance_variable_get('@method_name').should == 'foo'
   end
 end
