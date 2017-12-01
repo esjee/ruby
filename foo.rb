8cdcd9ac92a91291a03bc76e82d58ed6948e61ff
esjee <ewfawefw3@gmail.com>
2017-12-01 23:46:10 +0100

    update manual test cases

--- foo.rb
+++ foo.rb
@@ -3,4 +3,6 @@ end
 
 class Foo
   def bar(a, b, c:, d: 1337); end
+
+  def bar2(a:); end
 end
--- kerk.rb
+++ kerk.rb
@@ -1,17 +1,27 @@
 require_relative 'foo.rb'
 
 begin
+  # C function
   #File.open()
-  Foo.new.bar()
+
+  # Ruby function, multiple args
+  #Foo.new.bar()
+
+  # Ruby function, just missing kw_arg
+  Foo.new.bar2()
+
+  # Proc
+  #x = ->(a,b) {}
+  #x.call()
 rescue => e
   e
   puts "~~~~~~~~~"
   recv = e.instance_variable_get("@receiver");
   meth = e.instance_variable_get("@method_name");
 
-  puts recv
-  puts meth
-  puts recv && meth && recv.method(meth).parameters
+  puts "recv: #{recv}"
+  puts "method: #{meth}"
+  p recv && meth && recv.method(meth).parameters
   puts "^^^^^^^^"
   fail
 end
